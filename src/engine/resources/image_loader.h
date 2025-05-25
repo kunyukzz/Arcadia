716601751c5e12e2293bca027cc492d2a7ce6bcf
kunyukzz <aan13prasetya@gmail.com>
2025-05-25 11:42:56 +0700

    add resources loader

---
+++ src/engine/resources/image_loader.h
@@ -0,0 +1,8 @@
+#ifndef __LOADER_IMAGE_H__
+#define __LOADER_IMAGE_H__
+
+#include "engine/systems/resource_sys.h"
+
+resource_loader_t loader_image_rsc_init(void);
+
+#endif //__LOADER_IMAGE_H__
---
+++ src/engine/resources/text_loader.h
@@ -0,0 +1,8 @@
+#ifndef __LOADER_TEXT_H__
+#define __LOADER_TEXT_H__
+
+#include "engine/systems/resource_sys.h"
+
+resource_loader_t loader_text_rsc_init(void);
+
+#endif //__LOADER_TEXT_H__
