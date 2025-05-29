746fae4666e44c15e889e732d70e1f928190f852
kunyukzz <aan13prasetya@gmail.com>
2025-05-29 18:45:07 +0700

    UI Shader (test 3 - not fix)

--- src/engine/renderer/renderer_be.c
+++ src/engine/renderer/renderer_be.c
@@ -10,14 +10,25 @@ b8 renderer_be_init(render_backend_type_t type, render_backend_t *backend) {
 	if (type == BACKEND_VULKAN) {
 		backend->init = vk_backend_init;
 		backend->begin_frame = vk_backend_begin_frame;
-		backend->update_global = vk_backend_update_global;
 		backend->end_frame = vk_backend_end_frame;
 		backend->resize = vk_backend_resize;
 		backend->shut = vk_backend_shut;
-		backend->update_obj = vk_backend_update_obj;
+
+		backend->update_world = vk_backend_update_world;
+		backend->update_ui = vk_backend_update_ui;
+
+		backend->begin_renderpass = vk_backend_begin_renderpass;
+		backend->end_renderpass = vk_backend_end_renderpass;
 
 		backend->init_tex = vk_backend_tex_init;
 		backend->shut_tex = vk_backend_tex_shut;
+
+		backend->init_material = vk_backend_material_init;
+		backend->shut_material = vk_backend_material_shut;
+
+		backend->draw_geometry = vk_backend_geo_render;
+		backend->init_geo = vk_backend_geometry_init;
+		backend->shut_geo = vk_backend_geometry_shut;
 		return true;
 	}
 	return false;
@@ -31,12 +42,23 @@ void renderer_be_shut(render_backend_t *backend) {
 
 	backend->init = 0;
 	backend->begin_frame = 0;
-	backend->update_global = 0;
 	backend->end_frame = 0;
 	backend->resize = 0;
 	backend->shut = 0;
-	backend->update_obj = 0;
+
+	backend->update_world = 0;
+	backend->update_ui = 0;
+
+	backend->begin_renderpass = 0;
+	backend->end_renderpass = 0;
 
 	backend->init_tex = 0;
 	backend->shut_tex = 0;
+
+	backend->init_material = 0;
+	backend->shut_material = 0;
+
+	backend->draw_geometry = 0;
+	backend->init_geo = 0;
+	backend->shut_geo = 0;
 }
--- src/engine/renderer/renderer_fe.h
+++ src/engine/renderer/renderer_fe.h
@@ -10,10 +10,17 @@ void renderer_resize(uint32_t width, uint32_t height);
 b8 renderer_draw_frame(render_packet_t *packet);
 void renderer_set_view(mat4 view);
 
-void renderer_tex_init(const char *name, int32_t width,
-                       int32_t height, int32_t channel_count,
-                       const uint8_t *pixel, b8 has_transparent,
-                       texture_t *texture);
+void renderer_tex_init(const uint8_t *pixel, texture_t *texture);
 void renderer_tex_shut(texture_t *texture);
+
+b8 renderer_material_init(material_t *material);
+void renderer_material_shut(material_t *material);
+
+b8   renderer_geometry_init(geometry_t *geometry, uint32_t vertex_size,
+                            uint32_t vertex_count, const void *vertices,
+                            uint32_t idx_size, uint32_t idx_count,
+                            const void *indices);
+
+void renderer_geometry_shut(geometry_t *geometry);
 
 #endif //__RENDERER_FRONTEND_H__
